034190b7c5974b64dd20dcdc59126d450b8802e8
EmmanuelSan01 <114524605+EmmanuelSan01@users.noreply.github.com>
2025-03-18 23:10:58 -0500

    Add files via upload

---
+++ Java/src/com/ese/sesion01/Exponencial.java
@@ -0,0 +1,33 @@
+package com.ese.sesion01;
+
+/**
+ *
+ * @author camper
+ */
+public class Exponencial {
+    public double calculo (int x) {
+        double sum = 0;
+        Factorial fact = new Factorial();
+        
+        for(int k=0; k<=10; k++) {
+            sum += Math.pow(x, k) / (double)fact.calculo(k);
+        }
+        return sum;
+    }
+}
+
+class PruebaFactorial {
+    public static void main(String[] args) {
+        Exponencial exp = new Exponencial();
+        int x = 1;
+        double result = exp.calculo(x);
+        System.out.println("exp(" + x + ") = " + result);
+        double expX = Math.exp(x);
+        if (Math.abs(expX - result) <= 0.0001) {
+            System.out.println("Precisión encontrada. " + Math.abs(expX - result));
+        } else {
+            System.out.println("Precisión no encontrada. " + Math.abs(expX - result));
+        }
+        // exp(1) = 2,718281828...
+    }
+}
---
+++ Java/src/com/ese/sesion01/HolaMundo.java
@@ -0,0 +1,11 @@
+package com.ese.sesion01;
+
+/**
+ *
+ * @author camper
+ */
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
