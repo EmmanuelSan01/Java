ec1271c510589236b021d06aeb0dddd9ec5dc40b
EmmanuelSan01 <114524605+EmmanuelSan01@users.noreply.github.com>
2025-03-19 16:57:48 -0500

    Add files via upload

---
+++ Java/src/sesion03/CicloForImpares.java
@@ -0,0 +1,17 @@
+package sesion03;
+
+/**
+ *
+ * @author camper
+ */
+public class CicloForImpares {
+    public static void main(String[] args) {
+        // Imprimir los impares que hay desde el 30 hasta el 0
+        for(int i=29; i>0; i-=2) {
+            // if(i%5==0) continue;
+            System.out.print(i + ", ");
+            // if(i<=10) break;
+        }
+        System.out.println("");
+    }
+}
---
+++ Java/src/sesion03/CicloForPares.java
@@ -0,0 +1,21 @@
+package sesion03;
+
+/**
+ *
+ * @author camper
+ */
+public class CicloForPares {
+    public static void main(String[] args) {
+        // Imprimir los primeros 10 números pares
+        for(int p=1; p<11; p++) {
+            int par = 2*(p-1);
+            System.out.print(par + ", ");
+        }
+        System.out.println("");
+        
+        for (int p=0; p<19; p+=2) {
+            System.out.print(p + ", ");
+        }
+        System.out.println("");
+    }
+}
